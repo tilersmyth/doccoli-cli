70a17490817c350f60fe040f4d0463d483c2133e
Tyler Smith <tyler.smith.la@gmail.com>
2018-12-16 08:33:03 -0500

    Activate file loop with project key pulled from config file

--- src/project-publish/PublishProjectFiles.ts
+++ src/project-publish/PublishProjectFiles.ts
@@ -2,6 +2,7 @@ import { PublishApi } from "../api/PublishApi";
 import { NodeGit } from "../lib/NodeGit";
 
 import keytar from "../utils/keytar";
+import { UndocFile } from "../utils/UndocFile";
 /**
  * Publish project files to server
  */
@@ -18,17 +19,16 @@ export class PublishProjectFiles {
       const git = new NodeGit();
       const commit = await git.lastCommit();
       const branch = await git.branch();
+      const config = await UndocFile.config();
 
-      await new PublishApi(token).results(
-        "446bc00c-a1a8-48d3-8595-8e217b59167b",
-        this.files[0],
-        { sha: commit.sha(), branch },
-        { size: 4, index: 0 }
-      );
-
-      // for (const file of this.files) {
-      //   await new PublishApi(token).results(file, commit.sha(), branch);
-      // }
+      for (let i = 0; i < this.files.length; i++) {
+        await new PublishApi(token).results(
+          config.key,
+          this.files[i],
+          { sha: commit.sha(), branch },
+          { size: this.files.length, index: i + 1 }
+        );
+      }
     } catch (err) {
       throw err;
     }
